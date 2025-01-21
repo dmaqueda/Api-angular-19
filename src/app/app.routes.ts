6fe17fce4f70ae505a75a3dc5c1a90eb5b88e642
dmaqueda <dmaqueda05@gmail.com>
2025-01-21 11:25:02 +0100

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { PracticaComponent } from './practica/practica.component';
+
+export const routes: Routes = [
+    {path: 'practica' , component: PracticaComponent},
+];
+
